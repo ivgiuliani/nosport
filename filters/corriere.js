84760f7e2079b364af75e5dfa1509ec70d0521e5
Ivan Giuliani <giuliani.v@gmail.com>
2010-11-26 21:31:39 +0100

    Fixed missing quotes

--- filters/corriere.js
+++ filters/corriere.js
@@ -2,7 +2,7 @@ var item = $(".edizione-grafica:first")
 if (item && item.length > 0) {
 	item.hide();
 	chrome.extension.sendRequest({
-		"type": hidcontent,
+		"type": "hidcontent",
 		"website": "www.corriere.it"
 	});
 }
