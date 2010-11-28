343bc9e912329af95e67ce34c98345f5f6770bd7
Ivan Giuliani <giuliani.v@gmail.com>
2010-11-28 20:59:14 +0100

    Another rule for corriere.it

--- filters/corriere.js
+++ filters/corriere.js
@@ -1,8 +1,23 @@
-var item = $(".edizione-grafica:first")
-if (item && item.length > 0) {
-	item.hide();
-	chrome.extension.sendRequest({
-		"type": "hidcontent",
-		"website": "www.corriere.it"
-	});
-}
+$(document).ready(function() {
+	var item1 = $(".edizione-grafica:first");
+	var item2 = $(".tr-home-serie-a");
+	var hidden = false;
+	
+	if (item1 && item1.length > 0) {
+		item1.hide();
+		hidden = true;
+	}
+	
+	if (item2 && item2.length > 0) {
+		item2 = item2.parent();
+		item2.hide();
+		hidden = true;
+	}
+	
+	if (hidden) {
+		chrome.extension.sendRequest({
+			"type": "hidcontent",
+			"website": "www.corriere.it"
+		});
+	}
+});
