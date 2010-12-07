a061f191baadad2941ee3d88241a7ff3bee47974
Ivan Giuliani <giuliani.v@gmail.com>
2010-12-07 21:40:51 +0100

    Updated rules for corriere.it

--- filters/corriere.js
+++ filters/corriere.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var item1 = $(".edizione-grafica:first");
 	var item2 = $(".tr-home-serie-a");
+	var item3 = $(".tr-home-champions");
 	var hidden = false;
 	
 	if (item1 && item1.length > 0) {
@@ -11,6 +12,12 @@ $(document).ready(function() {
 	if (item2 && item2.length > 0) {
 		item2 = item2.parent();
 		item2.hide();
+		hidden = true;
+	}
+
+	if (item3 && item2.length > 0) {
+		item3 = item3.parent();
+		item3.hide();
 		hidden = true;
 	}
 	
