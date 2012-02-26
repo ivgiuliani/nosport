e97886f0abc0ad9dd27110820325bdcbac3d01e3
Ivan Giuliani <giuliani.v@gmail.com>
2012-02-26 23:16:37 +0100

    Remove giant soccer box from homepage on repubblica.it

--- filters/repubblica.js
+++ filters/repubblica.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-	var item = $("#dirette-dashboard");
-	if (item && item.length > 0) {
-		item.hide();
+  var hidden = false;
+
+	var panel = $("#dirette-dashboard");
+  var box = $("div.riapertura-large");
+
+	if (panel && panel.length > 0) {
+		panel.hide();
+    hidden = true;
+  }
+
+  if (box && box.length > 0) {
+    var boxpanel = box.first("h5:contains('Serie A')");
+    if (boxpanel && boxpanel.length > 0) {
+      box.hide();
+      hidden = true;
+    }
+  }
+
+  if (hidden) {
 		chrome.extension.sendRequest({
 			"type": "hidcontent",
 			"website": "www.repubblica.it"
